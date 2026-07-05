62774c28033b590ec8269601efead807640c5c92
shepherl <geekfachusa@gmail.com>
2026-07-05 09:41:59 +0300

    1.8 для теста

--- app/src/main/java/org/example/ota/Updater.java
+++ app/src/main/java/org/example/ota/Updater.java
@@ -36,7 +36,7 @@ import javax.swing.SwingUtilities;
 public class Updater {
 
     private static final String GITHUB_REPO = "shepherl/kvnfaq"; 
-    public static final String CURRENT_VERSION = "1.2"; 
+    public static final String CURRENT_VERSION = "1.8"; 
 
     private static String getIconPathForScript() {
         String appPath = getAppPath();
